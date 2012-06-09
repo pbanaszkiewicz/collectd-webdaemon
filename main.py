35649b155b86d998ad325e491e58895f4dc44a0a
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2012-06-09 15:23:55 +0200

    In preparation for reading RRD files...

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tornado.ioloop
 import tornado.web
 import os
 import simplejson as json
+import rrdtool
 
 
 def filter_dirs(name):
@@ -11,25 +12,6 @@ def filter_dirs(name):
         if name.startswith(i):
             return True
     return False
-
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
-
-
-class TimeHandler(tornado.web.RequestHandler):
-    """
-    This handler simply prints out current EPOCH time (ie. seconds since
-    01/01/1970 00:00:00)
-    Why? Well, RRD files have time specified, but it's in EPOCH format. Thus
-    the browser must calculate the delta between user's system time and the time
-    printed out by this Handler. This delta will then be used to show those
-    nice looking graphs with accurate time on time-axis.
-    """
-    def get(self):
-        import time
-        self.write(str(int(time.time())))
 
 
 class ListHosts(tornado.web.RequestHandler):
@@ -73,13 +55,45 @@ class ListRRDs(tornado.web.RequestHandler):
             raise tornado.web.HTTPError(404, "Collectd metrics `%s@%s` were not found" % (metrics, host))
 
 
+class GetData(tornado.web.RequestHandler):
+    def get(self, host, metrics, rrd, start=None, end=None):
+        if not filter_dirs(metrics):
+            raise tornado.web.HTTPError(404, "Illegal collectd metrics directory `%s`" % metrics)
+
+        try:
+            data_dir = self.application.settings["collectd_directory"]
+            data_dir = os.path.join(data_dir, "rrd")
+            host_dir = os.path.join(data_dir, host)
+            metrics_dir = os.path.join(host_dir, metrics)
+            rrd_file = os.path.join(metrics_dir, rrd)
+
+            # TODO: improve reading
+            data = rrdtool.fetch(rrd_file)
+            self.write(json.dumps(data))
+
+        except OSError:
+            raise tornado.web.HTTPError(404, "Collectd metrics `%s` were not found" % (rrd_file, ))
+
+        except rrdtool.error, e:
+            raise tornado.web.HTTPError(500, "RRDTool is borked: `%s`" % str(e))
+
+
+class Threshold(tornado.web.ReqeustHandler):
+    """
+    Set and get current thresholds for given RRD
+    """
+    pass
+
+
 application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/time", TimeHandler),
-    (r"/list_hosts", ListHosts),
+    (r"/", tornado.web.RedirectHandler, {"url": "/page/index.html"}),
     (r"/page/(.*)", tornado.web.StaticFileHandler, {"path": "./page"}),
+    (r"/list_hosts", ListHosts),
     (r"/host/(.*)/list_metrics", ListMetrics),
     (r"/host/(.*)/list_rrds/(.*)", ListRRDs),
+    (r"/get/(.*)/(.*)/(.*)/(\d*)/?(\d*)/?", GetData),
+    (r"/threshold/(.*)/(.*)/(.*)", Threshold),
+    # it's likely that any other URL will be used for configuration protocol
 ],
 collectd_directory="/var/lib/collectd/",
 debug=True)

d1cddf2ec79f844d5fec2c36a311c9e4a117767c
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2012-06-21 21:47:47 +0200

    Beginning on the thresholds

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ settings = {
     "DEBUG": True,
     "debug_address": ["0.0.0.0", 8888],
     "collectd_directory": "/var/lib/collectd/",
-    "collectd_threshold_file": "/etc/collectd/threshold.conf",
+    "collectd_threshold_file": "thresholds.conf",
     "GWM_host": ["127.0.0.1", ],
 }
---
+++ static/js/thresholds.js
@@ -0,0 +1,30 @@
+var address = "";
+
+$("#btn_connect").click(function() {
+    console.log("connecting");
+    var server = $("#server").val();
+    var port = $("#port").val();
+
+    if (port) {
+        address = server + ":" + port;
+    } else {
+        address = server
+    }
+
+    // get thresholds configuration
+    $.get(address + "/threshold/", function(data) {
+        console.log("got some data: " + data);
+    }, "xml");
+    //$.getJSON(address + "/thresholds/", function(data) {
+    //    console.log("got some data: " + data);
+    //    $("#menu_select_host").removeClass("hidden");
+
+    //    // add to the #select_host <select>
+    //    $("#select_host").empty();
+    //    $.each(data, function(k, val) {
+    //        console.log("val: " + val);
+
+    //        $("<option/>", {html: val}).appendTo("#select_host");
+    //    });
+    //});
+});

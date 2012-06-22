cd50dcc854c575e9d775f99b1b3e7d5529fe2531
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2012-06-22 10:01:58 +0200

    Slightly refactoring config converting functions

--- utils.py
+++ utils.py
@@ -9,24 +9,48 @@ def filter_dirs(name):
     return False
 
 
-def collectd_to_XML(string):
+def collectd_to_XML(config):
     """
     Change collectd's configuration format to XML (because they're very similar).
+
+    :param config:  Collectd's configuration
     """
-    S = string
-    S = re.sub(r'#.*$', r'', S, flags=re.U | re.I | re.M)  # drop comments
-    S = re.sub(r'^\s*$', r'', S, flags=re.U | re.I | re.M)  # drop empty lines
-    S = re.sub(r'<(\w+) "([^"]+)">', r'<\1 name="\2">', S, flags=re.U | re.I)
-    S = re.sub(r"^(\s+)(\w+)\s+(.+)$", r"\1<\2>\3</\2>", S, flags=re.M | re.U | re.I)
-    S = re.sub(r'>"([^"]+)"<', r'>\1<', S)
+    S = config
+
+    replaces = [
+        [r'#.*$', r'', re.M],  # drop comments
+        [r'^\s*$', r'', re.M],  # drop empty lines
+        [r'<(\w+) "([^"]+)">', r'<\1 name="\2">', None],  # add attribute `name`
+        [r"^(\s+)(\w+)\s+(.+)$", r"\1<\2>\3</\2>", re.M],  # change Value XYZ -> <Value>XYZ</Value>
+        [r'>"([^"]+)"<', r'>\1<', None],  # "asd" -> asd
+        [r"\s*<([^ >]+)([^>]*)>\r?\n\s*</\1>", r'<\1\2 />', None],  # combine empty entries <a></a> -> <a />
+    ]
+
+    for r1, r2, flags in replaces:
+        S = re.sub(r1, r2, re.U | re.I | (flags if flags else 0))
+
     return S
 
 
-def XML_to_collectd(string, spacing="  "):
+def XML_to_collectd(config, spacing="  "):
     """
     Change XML to collectd's configuration format.
+
+    :param config:   XML-ified collectd's configuration
+    :param spacing:  default gap between argument name and its value
     """
-    S = re.sub(r'<(\w+) name="([^"]+)">', r'<\1 "\2">', string, flags=re.U | re.I)
+    S = config
+
+    # remove attribute `name`
+    S = re.sub(r'<(\w+) name="([^"]+)">', r'<\1 "\2">', S, flags=re.U | re.I)
+
+    # change <Value>XYZ</Value> -> Value/:param :spacing/XYZ
     S = re.sub(r'<(\w+)>([^<]+)</\1>', r'\1' + spacing + r'\2', S, flags=re.U | re.I)
+
+    # add "" around non-digit values
     S = re.sub(r'^(\s+)(\w+)(\s+)([a-z].+)$', r'\1\2\3"\4"', S, flags=re.U | re.I | re.M)
+
+    # drop empty entries
+    S = re.sub(r'\s*<([^>]+)/>', r'', S, flags=re.U | re.I)  # drop empty entries
+
     return S
